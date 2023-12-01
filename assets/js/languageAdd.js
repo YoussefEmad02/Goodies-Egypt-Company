431c5aebce1c6ac41723312b2a6d2f4c4cf49be4
YoussefEmad02 <yousefemad2002@icloud.com>
2023-12-01 23:06:40 +0200

    Arabic added

---
+++ assets/js/languageAdd.js
@@ -0,0 +1,201 @@
+const translations = {
+  en: {
+    home: "Home",
+    ourstory: "Our Story",
+    contact: "Contact us",
+    selectLanguage: "Select a language",
+    english: "English",
+    arabic: "Arabic",
+    aboutus:"AboutUs",
+    ourstory:"Our story",
+    mission:"Mission",
+    vision:"vision",
+    media:"Media",
+    careers:"Careers",
+    jointeam:"Join Our Team",
+    contactus:"Contact Us",
+    applicationlabel:"Application Form",
+    career:"career",
+    personallabel:"Personal Detail",
+    namelabel:"Your Name",
+    genderlabel:"Select Your Gender",
+    male:"Male",
+    female:"Female",
+    gender:"Gender Type",
+    jobdomain:"Job Domain",
+    jobtype:"Select Job Type",
+    job1:"Job 1",
+    job2:"Job 2",
+    maillabel:"Your Email Address",
+    datelabel:"Date of Birth",
+    resumeatt:"Attach resume",
+    proposal:"Your proposal",
+    applynow:"Apply Now",
+    welcome:"WELCOME TO GOODIES Egypt Company",
+    upperp:"Providing the best types of natural food products, including Egyptian cheese, mozzarella, Lebanese and Syrian pastries, and frozen appetizers",
+    tastenow:"Taste now",
+    ourbrand:"Our Brands",
+    brand1:"Brand1",
+    brand2:"Brand2",
+    brand3:"Brand3",
+    visitbrand:"Visit Brand",
+    goodies:"Goodies",
+    destination:"Your Destination for Goodies",
+    ourstorypar:`Goodies Egypt is a Lebanese-Egyptian company specialized in the food industry. It was founded in 1999 by a
+    Lebanese investor in Egypt. It owns a variety of food brands, including the “Chtaura” brand, which was named
+    after the famous ancient Lebanese city. With natural cheese and dairy products.
+    The brand (Chtaura) offers a range of Syrian, Lebanese and international cheeses, in addition to a variety
+    of appetizers and frozen pastes such as goulash (baklava chips), samosas of various types, and many others.
+    Goodies Egypt also owns the brand (Ritaj), which meets the needs of all customers with a different flavor.
+    In addition, Goodies Egypt began launching a series of exhibitions under the brand (Goodies Patisserie),
+    which specializes in producing all types of Eastern and Western sweets and the finest types of pastries.
+    Goodies Egypt is planning to launch more brands to serve consumers from different categories.
+    We at Goodies Egypt are committed to providing healthy food that conforms to the highest international
+    standards and specifications. Our factories are equipped with the latest machines and a team of highly
+    qualified professionals, as we have obtained many quality certificates, including the ISO 22000/2018
+    certificate, which ensures that our manufacturing processes and food safety procedures comply with global
+    standards.
+    We do not compromise on quality, and implement strict quality management programs for all our raw materials,
+    ensuring that only premium products reach our customers.
+    We also use packaging materials that adhere to approved food standards, underscoring our commitment to
+    excellence.
+    We understand the importance of maintaining product integrity throughout the supply chain, so we have a
+    fleet of vehicles with precise temperature control.
+    These vehicles are designed to transport our products according to specific storage grades, ensuring that
+    our customers receive quality goods.
+    Goodies Egypt continues to set new standards through its focus on quality, authenticity and customer
+    satisfaction.
+    Thanks to our rich history and unwavering commitment to excellence, we aim to be the preferred choice for
+    natural Lebanese cheese products in Egypt and abroad.
+    We are proud to provide the authentic taste that delights the senses.`,
+    byfam:"By FAMILIES",
+    mispar:`Our ultimate goal is to provide healthy food for our children while actively participating in creating a
+    more prosperous world.
+    Goodies Egypt is committed to enhancing the well-being of Egyptian families by achieving a prominent
+    position in the market for
+    Syrian, Lebanese, and Egyptian cheese, appetizers, pastries, and halal meat products. This achievement
+    is achieved through the
+    use of the latest technologies in our production, transportation, and distribution processes, as well as
+    through the continuous
+    development of our workforce and unwavering adherence to stringent food quality standards.`,
+    visionpar:`Providing the best types of natural food products, including Egyptian cheese, mozzarella, Lebanese and
+    Syrian pastries, and
+    frozen appetizers, completely safe for the health of our children and manufactured with the latest
+    machines and technology,
+    and with the highest international quality standards, in order to contribute to achieving food security
+    for the Egyptian
+    family.`,
+    export:"Export",
+    exportpar:`Exploring Global Markets, Savoring Triumphs.
+    As a prominent diversified food enterprise in the Middle East and Egypt, Goodies Egypt delivers services
+    adhering to international quality standards.
+    Our commitment to excellence is evident in the promotion of high-quality & diverse categories of
+    products, empowering your business with a range of various product options that translate into assured
+    and diverse profits.`,
+    products:"Products and services",
+    mediaandnews:"Media and News",
+    contactpar:"Any question or remarks? Just write us a message!",
+    contactinfo:"Contact Information",
+    livechat:"Say something to start a live chat!",
+    firstname:"First Name",
+    lastname:"Last Name",
+    name:"Name",
+    phone:"Phone Number",
+    subject:"Select subject?",
+    option1:"option1",
+    option2:"option2",
+    option3:"option3",
+    option4:"option4",
+    option5:"option5",
+    message:"Message",
+    sendmessage:"Send Message",
+  },
+  ar: {
+    home: "الصفحة الرئيسية",
+    ourstory: "قصتنا",
+    contact: "تواصل معنا",
+    selectLanguage: "إختر لغة",
+    english: "الانجليزية",
+    arabic: "العربية",
+    aboutus:"من نحن",
+    ourstory:"قصتنا",
+    mission:"مهمتنا",
+    vision:"رؤيتنا",
+    media:"الوسائط",
+    careers:"الوظائف",
+    contactus:"تواصل معنا",
+    applicationlabel:"نموذج الطلب",
+    personallabel:"البيانات الشخصية",
+    jointeam:"انضم لنا",
+    namelabel:"الاسم",
+    genderlabel:"اختر النوع",
+    male:"ذكر",
+    female:"انثى",
+    gender:"النوع",
+    jobdomain:"المجال الوظيفي",
+    jobtype:"حدد نوع الوظيفة",
+    job1:"الوظيفة 1",
+    job2:"الوظيفة 2",
+    maillabel:"عنوان البريد الإلكتروني",
+    datelabel:"تاريخ الميلاد",
+    resumeatt:"أرفق السيرة الذاتية",
+    proposal:"اقتراحك",
+    applynow:"قدم الآن",
+    welcome:"مرحبا بكم في شركة جوديز مصر",
+    upperp:"نقدم أفضل أنواع المنتجات الغذائية الطبيعية من أجبان مصرية و موزريلا و معجنات لبنانية و سورية و المقبلات المجمدة",
+    tastenow:"تذوق الآن",
+    ourbrand:"علاماتنا التجارية",
+    brand1:"العلامة التجارية1",
+    brand2:"العلامة التجارية2",
+    brand3:"العلامة التجارية3",
+    visitbrand:"الزيارة",
+    goodies:"جوديز",
+    destination:"وجهتك للأشياء الجيدة",
+    ourstorypar:`Goodies Egypt هي شركة لبنانية مصرية متخصصة في صناعة المواد الغذائية. تأسست عام 1999 على يد مستثمر لبناني في مصر. وتمتلك مجموعة متنوعة من العلامات التجارية للمواد الغذائية، بما في ذلك علامة “شتورة” التي سميت على اسم المدينة اللبنانية القديمة الشهيرة. مع الجبن الطبيعي ومنتجات الألبان. تقدم العلامة التجارية (شتورة) مجموعة من الأجبان السورية واللبنانية والعالمية، بالإضافة إلى مجموعة متنوعة من المقبلات والمعاجين المجمدة مثل الجلاش (رقائق البقلاوة) والسمبوسة بأنواعها المختلفة، وغيرها الكثير. كما تمتلك جوديز ايجيبت العلامة التجارية (ريتاج) التي تلبي احتياجات جميع العملاء بنكهة مختلفة. بالإضافة إلى ذلك بدأت شركة جوديز إيجيبت في إطلاق سلسلة من المعارض تحت العلامة التجارية (جوديز باتيسيري) المتخصصة في إنتاج جميع أنواع الحلويات الشرقية والغربية وأجود أنواع المعجنات. وتخطط Goodies Egypt لإطلاق المزيد من العلامات التجارية لخدمة المستهلكين من مختلف الفئات. نحن في جوديز مصر ملتزمون بتوفير غذاء صحي مطابق لأعلى المعايير والمواصفات العالمية. مصانعنا مجهزة بأحدث الآلات وفريق عمل من المهنيين المؤهلين تأهيلا عاليا، كما حصلنا على العديد من شهادات الجودة، بما في ذلك شهادة ISO 22000/2018، والتي تضمن أن عمليات التصنيع وإجراءات سلامة الأغذية لدينا تتوافق مع المعايير العالمية. نحن لا نتنازل عن الجودة، وننفذ برامج صارمة لإدارة الجودة لجميع المواد الخام لدينا، مما يضمن وصول المنتجات المتميزة فقط إلى عملائنا. كما أننا نستخدم مواد التعبئة والتغليف التي تلتزم بالمعايير الغذائية المعتمدة، مما يؤكد التزامنا بالتميز. نحن ندرك أهمية الحفاظ على سلامة المنتج في جميع أنحاء سلسلة التوريد، لذلك لدينا أسطول من المركبات ذات التحكم الدقيق في درجة الحرارة. تم تصميم هذه المركبات لنقل منتجاتنا وفقًا لدرجات تخزين محددة، مما يضمن حصول عملائنا على سلع عالية الجودة. تواصل Goodies Egypt وضع معايير جديدة من خلال تركيزها على الجودة والأصالة ورضا العملاء. بفضل تاريخنا الغني والتزامنا الثابت بالتميز، نهدف إلى أن نكون الخيار المفضل لمنتجات الجبن اللبنانية الطبيعية في مصر والخارج. نحن فخورون بتقديم الطعم الأصيل الذي يُبهج الحواس.`,
+    byfam:"من قبل العائلات",
+    mispar:`هدفنا النهائي هو توفير الغذاء الصحي لأطفالنا مع المشاركة الفعالة في خلق عالم أكثر ازدهارًا. تلتزم Goodies Egypt بتعزيز رفاهية الأسر المصرية من خلال تحقيق مكانة بارزة في سوق الجبن والمقبلات والمعجنات ومنتجات اللحوم الحلال السورية واللبنانية والمصرية. ويتم تحقيق هذا الإنجاز من خلال استخدام أحدث التقنيات في عمليات الإنتاج والنقل والتوزيع لدينا، وكذلك من خلال التطوير المستمر للقوى العاملة لدينا والالتزام الثابت بمعايير جودة الأغذية الصارمة.`,
+    visionpar:`تقديم أفضل أنواع المنتجات الغذائية الطبيعية من أجبان مصرية وموزاريلا ومعجنات لبنانية وسورية والمقبلات المجمدة الآمنة تماما على صحة أطفالنا ومصنوعة بأحدث الآلات والتكنولوجيا وبأعلى معايير الجودة العالمية في من أجل المساهمة في تحقيق الأمن الغذائي للأسرة المصرية.`,
+    export:"التصدير",
+    exportpar:`استكشاف الأسواق العالمية، وتذوق الانتصارات. باعتبارها مؤسسة غذائية بارزة ومتنوعة في الشرق الأوسط ومصر، تقدم جوديز مصر خدمات تلتزم بمعايير الجودة الدولية. إن التزامنا بالتميز واضح في الترويج لفئات المنتجات المتنوعة وعالية الجودة، وتمكين عملك بمجموعة من خيارات المنتجات المتنوعة التي تترجم إلى أرباح مضمونة ومتنوعة.`,
+    products:"منتجات وخدمات",
+    mediaandnews:"الوسائط والأخبار",
+    contactpar:"أي سؤال أو ملاحظات؟ فقط اكتب لنا رسالة!",
+    contactinfo:"بيانات الاتصال",
+    livechat:"قل شيئًا لبدء محادثة مباشرة!",
+    firstname:"الاسم الأول",
+    lastname:"الاسم الأخير",
+    name:"الاسم",
+    phone:"رقم الهاتف",
+    subject:"حدد الموضوع؟",
+    option1:"الخيار 1",
+    option2:"الخيار 2",
+    option3:"الخيار 3",
+    option4:"الخيار 4",
+    option5:"الخيار 5",
+    message:"رسالة",
+    sendmessage:"أرسل رسالة",
+  },
+};
+console.log("test");
+const languageSelector = document.getElementById("lanselect");
+if(languageSelector){
+languageSelector.addEventListener("change", (event) => {
+  setLanguage(event.target.value);
+  localStorage.setItem("lang", event.target.value);
+});
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const language = localStorage.getItem("lang") || "en";
+  setLanguage(language);
+});
+
+const setLanguage = (language) => {
+  const elements = document.querySelectorAll("[data-lan]");
+  elements.forEach((element) => {
+    const translationKey = element.getAttribute("data-lan");
+    element.textContent = translations[language][translationKey];
+  });
+  document.dir = language === "ar" ? "rtl" : "ltr";
+};
+
